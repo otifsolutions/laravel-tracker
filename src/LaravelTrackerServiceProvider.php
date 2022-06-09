40655cc51baaa992cf4aafdd33b1f92ec5ee020b
danish9811 <danish.mehmood@otif-solutions.com>
2022-06-09 19:05:41 +0500

    registered global middleware in service providers boot method

--- src/LaravelTrackerServiceProvider.php
+++ src/LaravelTrackerServiceProvider.php
@@ -2,15 +2,18 @@
 
 namespace OTIFSolutions\LaravelTracker;
 
+use App\Http\Kernel;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\ServiceProvider;
 use OTIFSolutions\LaravelTracker\Commands\RemoveOldData;
+use OTIFSolutions\LaravelTracker\Http\Middleware\TrackActivities;
 
 class LaravelTrackerServiceProvider extends ServiceProvider {
 
-    public function boot() {
+    public function boot(Kernel $kernel) {
 
         $this->loadMigrationsFrom(__DIR__ . '/Database/migrations');
+        $kernel->pushMiddleware(TrackActivities::class);
 
         if ($this->app->runningInConsole()) {
             $this->commands([
