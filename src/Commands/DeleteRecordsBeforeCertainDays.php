a716681a87980b38d7d6cbc75f44e76313b85b67
danish9811 <danish.mehmood@otif-solutions.com>
2022-06-23 18:54:26 +0500

    command added to remove unnecessary old records

--- src/Commands/DeleteRecordsBeforeCertainDays.php
+++ src/Commands/DeleteRecordsBeforeCertainDays.php
@@ -3,6 +3,8 @@
 namespace OTIFSolutions\LaravelTracker\Commands;
 
 use Illuminate\Console\Command;
+use OTIFSolutions\LaravelTracker\Models\OtifUser;
+use OTIFSolutions\LaravelTracker\Models\OtifUserActivity;
 
 class DeleteRecordsBeforeCertainDays extends Command {
 
@@ -14,9 +16,20 @@ class DeleteRecordsBeforeCertainDays extends Command {
 
     public function handle() {
 
+        // if user has given this key from anywhere else, this will be overritten
+        $keepDataExceptDays = Setting::get('keep_except') ?? Setting::set('keep_except', 10);
 
-        // logic goes here to remove old records
+        if (OtifUser::exists()) {
+            OtifUser::whereDate('created_at', '<=', Carbon::now()->subDays($keepDataExceptDays))->delete();
+            $this->info('Users data before ' . $keepDataExceptDays . ' days removed');
+            $this->newLine();
+        }
 
+        if (OtifUserActivity::exists()) {
+            OtifUserActivity::whereDate('created_at', '<=', Carbon::now()->subDays($keepDataExceptDays))->delete();
+            $this->info('Users activities before ' . $keepDataExceptDays . ' days removed');
+            $this->newLine();
+        }
 
         return 0;
     }
