53adfbb05f84706f3ba3538d0869aed4e6971b7f
danish9811 <danish.mehmood@otif-solutions.com>
2022-06-28 15:39:59 +0500

    changes made

--- src/Http/Middlewares/TrackActivities.php
+++ src/Http/Middlewares/TrackActivities.php
@@ -15,12 +15,27 @@ class TrackActivities {
 
     public function handle(Request $request, Closure $next) {
 
-        if ($this->trackerStatus) {
+        if ($this->getTrackerStatus()) {
 
             if (!OtifUser::where('ip_address', $request->getClientIp())->exists()) {
-                OtifUser::create([
-                    'name' => Auth::user()->name ?? null,
-                    'email' => Auth::user()->email ?? null,
+
+                if (!Auth::check()) {
+                    OtifUser::create([
+                        'name' => null,
+                        'email' => null,
+                        'session_id' => $request->session()->getId(),
+                        'ip_address' => $request->getClientIp(),
+                        'http_host' => $request->server->get('HTTP_HOST'),
+                        'browser' => $request->header('User-Agent')
+                    ]);
+                }
+            }
+
+            // if same guest is authed, new session is generated
+            if (Auth::check()) {
+                OtifUser::updateOrCreate(['name' => Auth::user()->name, 'email' => Auth::user()->email], [
+                    'name' => Auth::user()->name,
+                    'email' => Auth::user()->email,
                     'session_id' => $request->session()->getId(),
                     'ip_address' => $request->getClientIp(),
                     'http_host' => $request->server->get('HTTP_HOST'),
@@ -28,17 +43,24 @@ class TrackActivities {
                 ]);
             }
 
+            $userId = OtifUser::where('ip_address', $request->getClientIp())->latest()->first()->id;
+
             OtifUserActivity::create([
-                'user_id' => OtifUser::where('ip_address', $request->getClientIp())->first()['id'],
-                'session_id' => $request->session()->getId(),
+                'user_id' => $userId,
                 'full_url' => $request->fullUrl(),
                 'redirect_url' => $request->server->get('REDIRECT_URL'),
                 'request_method' => $request->server->get('REQUEST_METHOD'),
                 'redirect_status' => $request->server->get('REDIRECT_STATUS'),
-                'query_string' => $request->server->get('QUERY_STRING'),
                 'query_string_json' => json_encode(explode('?', $request->server->get('QUERY_STRING')), JSON_THROW_ON_ERROR)
             ]);
 
+            if (Auth::check()) {
+                OtifUserRequestData::create([
+                    'user_id' => $userId,
+                    'request_data' => json_encode($this->encodeRequest($request), JSON_THROW_ON_ERROR),
+                    'request_method' => $request->server->get('REQUEST_METHOD')
+                ]);
+            }
         }
 
         return $next($request);
