ca003fbcd596a1900d11b3492b0e41c1c3ef1086
danish9811 <danish.mehmood@otif-solutions.com>
2022-07-01 19:24:10 +0500

    added models

---
+++ src/Models/UserActivity.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OTIFSolutions\LaravelTracker\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserActivity extends Model {
+
+    protected $guarded = [];
+
+    public function novaSession() {
+        return $this->belongsTo(NovaSession::class);
+    }
+
+
+}
