79153a2b2d1bdbde767911049ef4477db09b34ca
danish9811 <danish.mehmood@otif-solutions.com>
2022-07-04 12:27:07 +0500

    added accessor and made relation with parent model

--- src/Models/MyCookie.php
+++ src/Models/MyCookie.php
@@ -3,15 +3,23 @@
 namespace OTIFSolutions\LaravelTracker\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class MyCookie extends Model {
 
     protected $guarded = [];
 
+    // accessor "cookies_data" colomn
+    protected function getCookiesDataAttribute($value) {
+        $newArray = [];
+        foreach (json_decode($value) as $key => $item) {
+            $newArray[$key] = Crypt::decryptString($item);
+        }
+        return $newArray;
+    }
+
     public function cookies() {
-        return $this->belongsTo(NovaSession::class);      // define relationship with session
+        return $this->belongsTo(NovaSession::class);
     }
 
 }
-
-
