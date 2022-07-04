a0c40e1298403e604d469563e66ec3e6a6d76ca9
danish9811 <danish.mehmood@otif-solutions.com>
2022-07-04 12:27:37 +0500

    minor changes

--- src/Models/NovaSession.php
+++ src/Models/NovaSession.php
@@ -8,15 +8,15 @@ class NovaSession extends Model {
 
     protected $guarded = [];
 
-    public function activities() {
-        return $this->hasMany(UserActivity::class, 'nova_session_id', 'id');
+    public function userActivities() {
+        return $this->hasMany(UserActivity::class);
     }
 
     public function requestData() {
         return $this->hasMany(RequestData::class, 'nova_session_id', 'id');
     }
 
-    public function cookies() {
+    public function cookiesData() {
         return $this->hasMany(MyCookie::class, 'nova_session_id', 'id');
     }
 
