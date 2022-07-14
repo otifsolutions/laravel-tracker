556cfbbec06b8517b3bd71f4ebfe0336d8c890ff
danish9811 <danish.mehmood@otif-solutions.com>
2022-07-14 15:18:34 +0500

    changed the arrangement and make tiney changes

--- src/Migrations/2022_06_22_120252_create_user_sessions_table.php
+++ src/Migrations/2022_06_22_120252_create_user_sessions_table.php
@@ -7,19 +7,19 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
 
     public function up() {
-        Schema::create('user_sessions', static function (Blueprint $table) {
+        Schema::create('user_sessions', function (Blueprint $table) {
             $table->engine = 'myIsam';
             $table->id();
             $table->unsignedInteger('auth_id')->nullable();
-            $table->string('session_id');   // the actual session id, it is a random string
+            $table->string('session_id');   // the actual sesssion_id, the random string
             $table->string('ip_address');
             $table->string('http_host');
             $table->string('http_accept');
-            $table->string('server_name');
-            $table->string('server_addr');
+            $table->string('server_name')->nullable();
+            $table->string('server_admin')->nullable();
+            $table->string('server_addr')->nullable();
+            $table->string('server_signature')->nullable();
             $table->string('remote_addr');
-            $table->string('server_admin');
-            $table->string('server_signature');
             $table->string('browser')->nullable();
             $table->timestamps();
         });
